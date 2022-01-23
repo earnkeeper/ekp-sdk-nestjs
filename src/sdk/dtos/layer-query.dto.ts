ebda2c8bba9ac82f905cb107de927397ac81ebca
gavin-shaw <gavin@gnarus.io>
2022-01-23 15:57:21 +0200

    change: refactor LayerQueryDto.timestamp

--- src/sdk/dtos/layer-query.dto.ts
+++ src/sdk/dtos/layer-query.dto.ts
@@ -1,8 +1,11 @@
 export interface LayerQueryDto {
   readonly id?: string;
   readonly timestamp?: {
-    readonly before: number;
-    readonly after: number;
+    readonly eq?: number;
+    readonly gt?: number;
+    readonly gte?: number;
+    readonly lt?: number;
+    readonly lte?: number;
   };
   readonly tags?: string[];
 }
